a2d20e8667289e9119d6a14a0cd600098aaceab5
Danny S <daniel.martin.sheehan@gmail.com>
2021-03-10 18:09:09 -0500

    add todo for some county thing that is going wrong

--- census_acs_to_sqlite.py
+++ census_acs_to_sqlite.py
@@ -32,6 +32,8 @@ class CensusTableRequest:
 
         self.variables_list = [
             f'{i}E' for i in variables_df['uniqueid'].unique()]
+        self.variables_list = [i if '_' in i else i[:6] + '_' + i[6:]
+                               for i in self.variables_list]
         # ToDo Add underscore for years that don't have it.
 
         census_data = CensusACS5Year(
@@ -46,7 +48,7 @@ class CensusTableRequest:
 
         if 'sqlite' in file_formats:
             engine = create_engine(
-                f'sqlite:///output/{table_name}.db', echo=False)
+                f'sqlite:///{self.out_dir}/{table_name}.db', echo=False)
             census_data.df.to_sql(
                 f'{self.table_id}', con=engine, if_exists='replace')
 
@@ -57,9 +59,10 @@ class CensusTableRequest:
 if __name__ == '__main__':
     config = read_json('config.json')
 
-    for census_year in [2015, 2016, 2017, 2018, 2019]:
-        for census_geog in ['county']:  # 'tract'
-            for topic in ['B19013', 'B25033']:
+    # 2015, 2016, 2017,
+    for census_year in [2018]: #, 2019]:
+        for census_geog in ['county']:  # state', 'tract']:  # 'county 'tract'  # ToDo - state and tract work, why not county?
+            for topic in ['B05001']:  # B01001', 'B19013', 'B25033',
                 CensusTableRequest(
                     config['api_key'], census_year, census_geog, topic,
                     'data/output',
